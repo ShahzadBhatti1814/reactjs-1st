e20e843fe0d468e0d2938cbdd8ed59042635d23c
ShahzadBhatti1814 <sb393310@gmail.com>
2024-09-08 02:02:39 -0700

    my new repo

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import "./App.css";
+function App(){
+	return (
+	  <div className="firstHeading">
+	SHAHZAD NASEER BHATTI
+	<h1>OBJECTIVE:</h1>
+	<p>To enhance my knowlage and capabilities by working in a dynamic organization that pride itself is giving substantial responsibility to new talent.</p>
+	<h1>PERSONAL INFORMATION:</h1>
+	<ul>
+		<li>Father's name : Naseer Ahmed</li>
+		<li>surname  :  Bhatti</li>
+		<li>Date of Birth  :  14-11-2002</li>
+		<li>Cnic No  :  38407-0368237-9</li>
+		<li>Religion  :  Islam</li>
+		<li>Domicile  :  District Sargodha</li>
+		<li>Nationality  :  Pakistan</li>
+		<li>Marital status  :  Single</li>
+		<li>Mobile No  :  03018847093</li>
+		</ul>
+	<h1>QUALIFICATION:</h1>
+	<ul>
+		<li>Matriculation from B.I.S.E Sargodha</li>
+		<li>Intermediate from B.I.S.E Sargodha</li>
+		<li>Graduation from B.I.S.E Sargodha</li>
+	</ul>
+	<h1>OTHERS SKILLS</h1>
+	<ul>
+	<li>6 Month  Web Development course </li>
+	</ul>
+	<h1>HOBBIES</h1>
+	<ul>
+		<li>Use Computer</li>
+		<li>Net working</li>
+		<li>Play Cricket</li>
+	</ul>
+	<h1>LANGUAGE</h1>
+	<ul>
+	<li>Urdu</li>
+	<li>English</li>
+	<li>Punjabi</li>
+	</ul>
+		</div>
+	)
+};
+
+export default App;
